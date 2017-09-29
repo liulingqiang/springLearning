cef90c9e35f51a9580702e0c310c624e3d34409a
liulq <liulq@szkingdom.com>
2017-09-29 10:09:39 +0800

    集成activiti modeler

---
+++ src/main/java/org/activiti/rest/editor/model/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.activiti.rest.editor.model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by liulq on 2017-09-28 .
+ */
+@Controller
+public class HelloWorld {
+
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String getHelloWorld(){
+        return "hello world";
+    }
+
+}
